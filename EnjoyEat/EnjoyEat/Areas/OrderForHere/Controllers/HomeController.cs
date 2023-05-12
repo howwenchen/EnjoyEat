95353669be6cbae8f4050bf3a28c4fc763ad886c
John <mouiu1177@gmail.com>
2023-05-12 11:16:11 +0800

    更新到最新檔案

---
+++ EnjoyEat/EnjoyEat/Areas/OrderForHere/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnjoyEat.Areas.OrderForHere.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
